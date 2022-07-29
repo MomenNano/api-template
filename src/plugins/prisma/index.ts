d13fcfbe468a2eee7593eaea22e62d9afbc0f960
Momen <momen@afnan-tech.com>
2022-07-29 07:50:04 +0200

    plugins, db: add primsa plugin

---
+++ src/plugins/prisma/index.ts
@@ -0,0 +1,32 @@
+import fp from 'fastify-plugin'
+import { FastifyPluginAsync } from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    db: PrismaClient
+  }
+}
+
+const prismaPlugin: FastifyPluginAsync = async (fastify, opts) => {
+  fastify.decorate('db', null)
+
+  const prisma = new PrismaClient()
+
+  try {
+    await prisma.$connect()
+    fastify.log.info('connectioned to DB')
+  } catch (error) {
+    fastify.log.fatal('failed to establish a connection with db')
+    throw error
+  }
+
+  fastify.db = prisma
+
+  fastify.addHook('onClose', async (fastify) => {
+    fastify.log.info('disconnecting Prisma from DB')
+    await fastify.db.$disconnect()
+  })
+}
+
+export default fp(prismaPlugin)
