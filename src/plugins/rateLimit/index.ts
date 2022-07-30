e1a13031f98b68b87523eb2c144a48043fda4eda
MomenNano <momennano1998@gmail.com>
2022-07-30 23:47:42 +0200

    plugin: add rate limit for api

---
+++ src/plugins/rateLimit/index.ts
@@ -0,0 +1,15 @@
+import { FastifyPluginAsync } from 'fastify'
+import fp from 'fastify-plugin'
+import fastifyRateLimit from '@fastify/rate-limit'
+
+const rateLimit: FastifyPluginAsync = async fastify => {
+  void fastify.register(fastifyRateLimit, {
+    timeWindow: '5 minutes',
+    max: 100,
+    whitelist: ['127.0.0.1', 'localhost'],
+    redis: fastify.redis,
+    skipOnError: true
+  })
+}
+
+export default fp(rateLimit)
